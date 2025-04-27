946b759aab42763875ff1335a056e98e2a76d2cf
lamonte24kG <william.golden@seattlecolleges.edu>
2025-04-26 17:45:21 -0700

    Add JSON dictionary assignment for LaMonte

---
+++ jsonIT112Spr25.py
@@ -0,0 +1,15 @@
+#import json library
+import json
+
+#create the json dictionary
+
+data = {
+
+    'name': 'LaMonte Golden',
+    'age': '54',
+    'city': 'Seattle,WA',
+    'interest': ['music', 'cooking', 'walks'],
+    'is_student': True
+
+
+}
