0bef394107b65c1429e55d4e8d002f6a6f8902c1
lamonte24kG <william.golden@seattlecolleges.edu>
2025-05-23 12:40:29 -0700

    Added JSON and Python script for IT112 assignment

--- jsonIT112Spr25.py
+++ jsonIT112Spr25.py
@@ -13,3 +13,9 @@ data = {
 
 
 }
+
+# Write the json dictionary to a file
+with open('dataIT112Spr25.json', 'w') as json_file:
+    json.dump(data, json_file, indent=4)
+
+print("You have successfully written to dataIT112Spr25.json")
